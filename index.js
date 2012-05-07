a0f9035382bb9117933e1e92b5f28f70d6348ded
TJ Peden <tj.peden@tj-coding.com>
2012-05-07 01:28:25 -0500

    Added comment, doing some prep work for nesting

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
+/*
+ * Express - Router
+ * Copyright(c) 2012 TJ Peden <tj.peden@tj-coding.com>
+ * MIT Licensed
+ * 
+ * Credit to TJ Holowaychuk and Daniel Gasienica for
+ * their work on express-resource.
+ */
+
+
+/**
+ * Module dependencies.
+ */
+ 
 var express = require('express');
 var fs = require('fs');
 var path = require('path');
 var lingo = require('lingo');
+
+/**
+ * Extend function.
+ */
 
 function $(destination, source) { // extend
   for(var property in source)
@@ -9,6 +27,11 @@ function $(destination, source) { // extend
       destination[property] = source[property];
   return destination;
 }
+
+/**
+ * Pre-defined action ordering.
+ * As in express-resource.
+ */
 
 var orderedActions = [
   'all',
@@ -20,6 +43,15 @@ var orderedActions = [
   'update',
   'destroy'
 ];
+
+/**
+ * Initialize a new `Resource` with the
+ * given `name` and `actions`.
+ * 
+ * @param {String} name
+ * @param {Object} actions
+ * @param {Server} app
+ */
 
 function Resource(name, actions, app) {
   this.name = actions.name || name;
@@ -33,6 +65,13 @@ function Resource(name, actions, app) {
 }
 
 $(Resource.prototype, {
+  
+  /**
+   * Configure the default actions.
+   * 
+   * @param {Object} actions
+   */
+  
   _init: function(actions) {
     var self = this;
     
@@ -63,11 +102,20 @@ $(Resource.prototype, {
           break;
       }
       
-      self.map(method, action, path, callback);
+      self.map(method, path, callback)
+        ._record(action, method, path);
     });
   },
+  
+  /**
+   * Return a generated path for the given action
+   * 
+   * @param {String} action
+   * @return {String}
+   */
+  
   path: function(action) {
-    var result = this.root ? '' : '/' + this.name;
+    var result = this.root ? '/' : '/' + this.name + '/';
     
     switch(action) {
       case 'all':
@@ -75,7 +123,7 @@ $(Resource.prototype, {
       case 'edit':
       case 'update':
       case 'destroy':
-        result += '/:' + this.id;
+        result += ':' + this.id;
       default: break;
     };
     
@@ -93,15 +141,53 @@ $(Resource.prototype, {
     
     return result;
   },
+  
+  /**
+   * Return the resource's default id string.
+   * 
+   * @return {String}
+   */
+  
   _defaultId: function() {
     return this.root ?
       'id' : lingo.en.singularize(this.name);
   },
-  map: function(method, action, path, callback) {
-    this.app[method](path, callback);
+  
+  /**
+   * Record the `method` and `path` a given `action`
+   * is mapped to. Also preserves order.
+   */
+  
+  _record: function(action, method, path) {
     method = method.toUpperCase();
-    this.routes.push({method: method, path: path, action: action});
+    this.routes.push({
+      action: action,
+      method: method,
+      path: path
+    });
   },
+  
+  /**
+   * Map http `method` and `path` to `callback`.
+   * 
+   * @param {String} method
+   * @param {String} path
+   * @param {Function} callback
+   * @return {Resource} for chaining
+   */
+  
+  map: function(method, path, callback) {
+    this.app[method](path, callback);
+    return this;
+  },
+  
+  /**
+   * Returns a rendering of all the routes mapped
+   * for this resource.
+   * 
+   * @return {String}
+   */
+  
   toString: function() {
     return this.routes.map(function(obj) {
       return obj.method + "\t" + obj.action + "\t" + obj.path;
@@ -109,14 +195,29 @@ $(Resource.prototype, {
   }
 });
 
-function Router(dir) {
+/**
+ * Initialize a new `ResourceManager` with a given `dir`.
+ * 
+ * @param {String} dir
+ */
+
+function ResourceManager(dir) {
   this.dir = dir;
   this.controllers = {};
 }
 
 $(Router.prototype, {
+  
+  /**
+   * Load all the controllers from `dir`
+   * then call `done`
+   * 
+   * @param {Function} done
+   */
+  
   initControllers: function(done) {
     var self = this;
+    
     fs.readdir(this.dir, function(error, files) {
       if(error) throw error;
       
@@ -127,27 +228,52 @@ $(Router.prototype, {
       done();
     });
   },
-  createRoutes: function(app) {
+  
+  /**
+   * Create new `Resources` for all of the controllers that were loaded.
+   * 
+   * @param {Server} app
+   */
+  
+  createResources: function(app) {
     app.resources = {};
+    
     for(var name in this.controllers) {
-      app.resources[name] =
-        new Resource(name, this.controllers[name], app);
+      if(!name in app.resources)
+        app.resources[name] =
+          new Resource(name, this.controllers[name], app);
     }
   }
 });
+
+/**
+ * Load and define resources for all the controllers in a given `dir`.
+ * 
+ * @param {String} dir
+ * @return {ResourceManager}
+ */
 
 express.HTTPServer.prototype.loadResources =
 express.HTTPSServer.prototype.loadResources = function(dir) {
   if(!dir) dir = path.join(__dirname, '..', '..', 'controllers');
   var self = this;
-  var router = new Router(dir);
+  var manager = new ResourceManager(dir);
   
-  router.initControllers(function() {
-    router.createRoutes(self);;
+  manager.initControllers(function() {
+    manager.createResources(self);
   });
   
-  return router;
+  return manager;
 };
+
+/**
+ * Define a resource with the given `name` and `actions`.
+ * Doin' it express-resource style.
+ * 
+ * @param {String} name
+ * @param {Object} actions
+ * @return {Resource}
+ */
 
 express.HTTPServer.prototype.resource =
 express.HTTPSServer.prototype.resource = function(name, actions) {
